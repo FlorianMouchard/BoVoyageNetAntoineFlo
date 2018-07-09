4862baa74206ab44f0d672472f65c4455ea18530
Antoine <>
2018-07-09 19:13:21 +0200

    Ajout Classe métier Voyage

---
+++ BoVoyageNetAntoineFlo/BoVoyageNetAntoineFlo/Models/Voyage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BoVoyageNetAntoineFlo.Models
+{
+    public class Voyage
+    {
+        public int IDVoyage { get; set; }
+
+        public DateTime DateAller { get; set; }
+
+        public DateTime DateRetour { get; set; }
+
+        public int PlacesDisponibles { get; set; }
+
+        public decimal TarifToutCompris { get; set; }
+    }
+}
