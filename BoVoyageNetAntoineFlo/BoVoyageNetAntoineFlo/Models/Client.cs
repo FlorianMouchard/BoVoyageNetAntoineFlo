1719ea9ab8bc42fbf8d4965741b134a0e8391fba
Antoine <>
2018-07-09 19:53:11 +0200

    Ajout Classe métier Client

---
+++ BoVoyageNetAntoineFlo/BoVoyageNetAntoineFlo/Models/Client.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BoVoyageNetAntoineFlo.Models
+{
+    public class Client
+    {
+        public int IDClient { get; set; }
+
+        public string Civilite { get; set; }
+
+        public string Nom { get; set; }
+
+        public string Prenom { get; set; }
+
+        public string Adresse { get; set; }
+
+        public string Telephone { get; set; }
+
+        public DateTime DateNaissance { get; set; }
+
+        public int Age { get; set; }
+
+        public string Email { get; set; }
+    }
+}
