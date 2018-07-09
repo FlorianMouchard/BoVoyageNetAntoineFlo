5df68f5ca211b8fd669110b4a55ab25844d513ca
Antoine <>
2018-07-09 19:08:57 +0200

    Ajout initial du projet (vide)

---
+++ BoVoyageNetAntoineFlo/BoVoyageNetAntoineFlo.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BoVoyageNetAntoineFlo.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ BoVoyageNetAntoineFlo/BoVoyageNetAntoineFlo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BoVoyageNetAntoineFlo.WebApiApplication" Language="C#" %>
