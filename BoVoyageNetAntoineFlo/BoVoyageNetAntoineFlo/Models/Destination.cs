5e5d8b8d79fb40aac44cfd46cd2f12869cd1243b
Antoine <>
2018-07-09 19:16:54 +0200

    Ajout Classe métier Destination

---
+++ BoVoyageNetAntoineFlo/BoVoyageNetAntoineFlo/Models/Destination.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BoVoyageNetAntoineFlo.Models
+{
+    public class Destination
+    {
+        public int IDDestination { get; set; }
+
+        public string Continent { get; set; }
+
+        public string Pays { get; set; }
+
+        public string Region { get; set; }
+
+        public string Description { get; set; }
+    }
+}
