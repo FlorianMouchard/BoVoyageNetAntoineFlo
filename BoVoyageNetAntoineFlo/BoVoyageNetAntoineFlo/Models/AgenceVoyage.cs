b66e3e98037c16e7892aef6f959f760fce6140a1
Florian Mouchard <florian.mouchard@sfr.fr>
2018-07-09 22:23:07 +0200

    Ajout de la classe AgenceVoyage

---
+++ BoVoyageNetAntoineFlo/BoVoyageNetAntoineFlo/Models/AgenceVoyage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BoVoyageNetAntoineFlo.Models
+{
+    public class AgenceVoyage
+    {
+        public int ID { get; set; }
+
+        public string Nom { get; set; }
+    }
+}
