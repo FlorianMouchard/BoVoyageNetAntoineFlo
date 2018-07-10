55e80f3807e67b48b8372d7057359ea96d5f4f78
Florian <florian.mouchard@sfr.fr>
2018-07-10 11:02:01 +0200

    Création des controller

---
+++ BoVoyageNetAntoineFlo/BoVoyageNetAntoineFlo/Controllers/DossierReservationsController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using BoVoyageNetAntoineFlo.Data;
+using BoVoyageNetAntoineFlo.Models;
+
+namespace BoVoyageNetAntoineFlo.Controllers
+{
+    public class DossierReservationsController : ApiController
+    {
+        private BoVoyageDbContext db = new BoVoyageDbContext();
+
+        // GET: api/DossierReservations
+        public IQueryable<DossierReservation> GetDossiersReservations()
+        {
+            return db.DossiersReservations;
+        }
+
+        // GET: api/DossierReservations/5
+        [ResponseType(typeof(DossierReservation))]
+        public IHttpActionResult GetDossierReservation(int id)
+        {
+            DossierReservation dossierReservation = db.DossiersReservations.Find(id);
+            if (dossierReservation == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(dossierReservation);
+        }
+
+        // PUT: api/DossierReservations/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutDossierReservation(int id, DossierReservation dossierReservation)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != dossierReservation.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(dossierReservation).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DossierReservationExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/DossierReservations
+        [ResponseType(typeof(DossierReservation))]
+        public IHttpActionResult PostDossierReservation(DossierReservation dossierReservation)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.DossiersReservations.Add(dossierReservation);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = dossierReservation.ID }, dossierReservation);
+        }
+
+        // DELETE: api/DossierReservations/5
+        [ResponseType(typeof(DossierReservation))]
+        public IHttpActionResult DeleteDossierReservation(int id)
+        {
+            DossierReservation dossierReservation = db.DossiersReservations.Find(id);
+            if (dossierReservation == null)
+            {
+                return NotFound();
+            }
+
+            db.DossiersReservations.Remove(dossierReservation);
+            db.SaveChanges();
+
+            return Ok(dossierReservation);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool DossierReservationExists(int id)
+        {
+            return db.DossiersReservations.Count(e => e.ID == id) > 0;
+        }
+    }
+}
